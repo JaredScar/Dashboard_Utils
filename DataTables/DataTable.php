f0fcd15d87ad5e37660c903e202ccc740cccebb9
Jared <TheWolfBadger@gmail.com>
2021-10-09 11:49:44 -0400

    Have a working wrapper so far, but still needs some fixes here and there

--- DataTables/DataTable.php
+++ DataTables/DataTable.php
@@ -5,62 +5,78 @@ class DataTable {
     private $headers;
     private $options;
     private $linkIncludes = [];
-    private $printedLinkIncludes = false;
+    private $installedLinkIncludes = false;
+    private $tableClasses = 'table table-striped table-hover';
+    private $data;
     private $scriptIncludes = [];
-    private $printedScriptIncludes = false;
+    private $installedScriptIncludes = false;
     private $style;
     public function __construct($identifier) {
         $this->identifer = $identifier;
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
-        $style = 'dataTables';
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->style = 'dataTables';
         return $this;
     }
     public function withBootstrap3() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap.min.js';
         $this->style = 'bootstrap';
         return $this;
     }
     public function withBootstrap4() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js';
         $this->style = 'bootstrap4';
         return $this;
     }
     public function withBootstrap5() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js';
         $this->style = 'bootstrap5';
         return $this;
     }
     public function withBulma() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bulma.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.bulma.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.bulma.min.js';
         $this->style = 'bulma';
         return $this;
     }
     public function withFoundation() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.foundation.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.foundation.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.foundation.min.js';
         $this->style = 'foundation';
         return $this;
     }
     public function withjQueryUI() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.jqueryui.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.jqueryui.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.jqueryui.min.js';
         $this->style = 'jqueryui';
         return $this;
     }
     public function withSemanticUI() {
-        $this->linkIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/css/dataTables.semanticui.min.css';
-        $this->scriptIncludes['DataTable'] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
+        $this->linkIncludes['DataTable'] = [];
+        $this->scriptIncludes['DataTable'] = [];
+        $this->linkIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/css/dataTables.semanticui.min.css';
+        $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js';
         $this->scriptIncludes['DataTable'][] = 'https://cdn.datatables.net/1.11.3/js/dataTables.semanticui.min.js';
         $this->style = 'semanticui';
         return $this;
@@ -70,16 +86,20 @@ class DataTable {
 
     public function extAutoFill() {
         $style = $this->style . ".";
-        $this->linkIncludes['AutoFill'] = 'https://cdn.datatables.net/autofill/2.3.7/css/autoFill.' . $style . 'min.css';
-        $this->scriptIncludes['AutoFill'] = 'https://cdn.datatables.net/autofill/2.3.7/js/dataTables.autoFill.min.js';
+        $this->linkIncludes['AutoFill'] = [];
+        $this->scriptIncludes['AutoFill'] = [];
+        $this->linkIncludes['AutoFill'][] = 'https://cdn.datatables.net/autofill/2.3.7/css/autoFill.' . $style . 'min.css';
+        $this->scriptIncludes['AutoFill'][] = 'https://cdn.datatables.net/autofill/2.3.7/js/dataTables.autoFill.min.js';
         if ($this->style != 'dataTables')
             $this->scriptIncludes['AutoFill'][] = 'https://cdn.datatables.net/autofill/2.3.7/js/autoFill.' . $style . 'min.js';
         return $this;
     }
     public function extButtons() {
         $style = $this->style . ".";
-        $this->linkIncludes['Buttons'] = 'https://cdn.datatables.net/buttons/2.0.1/css/buttons.' . $style . 'min.css';
-        $this->scriptIncludes['Buttons'] = 'https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js';
+        $this->linkIncludes['Buttons'] = [];
+        $this->scriptIncludes['Buttons'] = [];
+        $this->linkIncludes['Buttons'][] = 'https://cdn.datatables.net/buttons/2.0.1/css/buttons.' . $style . 'min.css';
+        $this->scriptIncludes['Buttons'][] = 'https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js';
         $this->scriptIncludes['Buttons'][] = 'https://cdn.datatables.net/buttons/2.0.1/js/buttons.colVis.min.js';
         $this->scriptIncludes['Buttons'][] = 'https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js';
         $this->scriptIncludes['Buttons'][] = 'https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js';
@@ -89,58 +109,81 @@ class DataTable {
     }
     public function extColReorder() {
         $style = $this->style . ".";
-        $this->linkIncludes['ColReorder'] = 'https://cdn.datatables.net/colreorder/1.5.4/js/dataTables.colReorder.min.js';
-        $this->scriptIncludes['ColReorder'] = 'https://cdn.datatables.net/colreorder/1.5.4/css/colReorder.' . $style . 'min.css';
+        $this->linkIncludes['ColReorder'] = [];
+        $this->scriptIncludes['ColReorder'] = [];
+        $this->linkIncludes['ColReorder'][] = 'https://cdn.datatables.net/colreorder/1.5.4/js/dataTables.colReorder.min.js';
+        $this->scriptIncludes['ColReorder'][] = 'https://cdn.datatables.net/colreorder/1.5.4/css/colReorder.' . $style . 'min.css';
         return $this;
     }
     public function extFixedColumns() {
-        $this->linkIncludes['FixedColumns'] = 'https://cdn.datatables.net/fixedcolumns/4.0.0/css/fixedColumns.dataTables.min.css';
-        $this->scriptIncludes['FixedColumns'] = 'https://cdn.datatables.net/fixedcolumns/4.0.0/js/dataTables.fixedColumns.min.js';
+        $this->linkIncludes['FixedColumns'] = [];
+        $this->scriptIncludes['FixedColumns'] = [];
+        $this->linkIncludes['FixedColumns'][] = 'https://cdn.datatables.net/fixedcolumns/4.0.0/css/fixedColumns.dataTables.min.css';
+        $this->scriptIncludes['FixedColumns'][] = 'https://cdn.datatables.net/fixedcolumns/4.0.0/js/dataTables.fixedColumns.min.js';
         return $this;
     }
     public function extFixedHeader() {
-        $this->linkIncludes['FixedHeader'] = 'https://cdn.datatables.net/fixedheader/3.2.0/css/fixedHeader.dataTables.min.css';
-        $this->scriptIncludes['FixedHeader'] = 'https://cdn.datatables.net/fixedheader/3.2.0/js/dataTables.fixedHeader.min.js';
+        $this->linkIncludes['FixedHeader'] = [];
+        $this->scriptIncludes['FixedHeader'] = [];
+        $this->linkIncludes['FixedHeader'][] = 'https://cdn.datatables.net/fixedheader/3.2.0/css/fixedHeader.dataTables.min.css';
+        $this->scriptIncludes['FixedHeader'][] = 'https://cdn.datatables.net/fixedheader/3.2.0/js/dataTables.fixedHeader.min.js';
         return $this;
     }
     public function extKeyTable() {
-        $this->linkIncludes['KeyTable'] = 'https://cdn.datatables.net/keytable/2.6.4/css/keyTable.dataTables.min.css';
-        $this->scriptIncludes['KeyTable'] = 'https://cdn.datatables.net/keytable/2.6.4/js/dataTables.keyTable.min.js';
+        $this->linkIncludes['KeyTable'] = [];
+        $this->scriptIncludes['KeyTable'] = [];
+        $this->linkIncludes['KeyTable'][] = 'https://cdn.datatables.net/keytable/2.6.4/css/keyTable.dataTables.min.css';
+        $this->scriptIncludes['KeyTable'][] = 'https://cdn.datatables.net/keytable/2.6.4/js/dataTables.keyTable.min.js';
         return $this;
     }
     public function extResponsive() {
-        $this->linkIncludes['Responsive'] = 'https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css';
-        $this->scriptIncludes['Responsive'] = 'https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js';
+        $this->linkIncludes['Responsive'] = [];
+        $this->scriptIncludes['Responsive'] = [];
+        $this->linkIncludes['Responsive'][] = 'https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css';
+        $this->scriptIncludes['Responsive'][] = 'https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js';
         return $this;
     }
     public function extRowGroup() {
-        $this->linkIncludes['RowGroup'] = 'https://cdn.datatables.net/rowgroup/1.1.3/css/rowGroup.dataTables.min.css';
-        $this->scriptIncludes['RowGroup'] = 'https://cdn.datatables.net/rowgroup/1.1.3/js/dataTables.rowGroup.min.js';
+        $this->linkIncludes['RowGroup'] = [];
+        $this->scriptIncludes['RowGroup'] = [];
+        $this->linkIncludes['RowGroup'][] = 'https://cdn.datatables.net/rowgroup/1.1.3/css/rowGroup.dataTables.min.css';
+        $this->scriptIncludes['RowGroup'][] = 'https://cdn.datatables.net/rowgroup/1.1.3/js/dataTables.rowGroup.min.js';
         return $this;
     }
     public function extRowReorder() {
-        $this->linkIncludes['RowReorder'] = 'https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css';
-        $this->scriptIncludes['RowReorder'] = 'https://cdn.datatables.net/rowreorder/1.2.8/js/dataTables.rowReorder.min.js';
+        $this->linkIncludes['RowReorder'] = [];
+        $this->scriptIncludes['RowReorder'] = [];
+        $this->linkIncludes['RowReorder'][] = 'https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css';
+        $this->scriptIncludes['RowReorder'][] = 'https://cdn.datatables.net/rowreorder/1.2.8/js/dataTables.rowReorder.min.js';
         return $this;
     }
     public function extScroller() {
-        $this->linkIncludes['Scroller'] = 'https://cdn.datatables.net/scroller/2.0.5/css/scroller.dataTables.min.css';
-        $this->scriptIncludes['Scroller'] = 'https://cdn.datatables.net/scroller/2.0.5/js/dataTables.scroller.min.js';
+        $this->linkIncludes['Scroller'] = [];
+        $this->scriptIncludes['Scroller'] = [];
+        $this->linkIncludes['Scroller'][] = 'https://cdn.datatables.net/scroller/2.0.5/css/scroller.dataTables.min.css';
+        $this->scriptIncludes['Scroller'][] = 'https://cdn.datatables.net/scroller/2.0.5/js/dataTables.scroller.min.js';
         return $this;
     }
     public function extSearchBuilder() {
-        $this->linkIncludes['SearchBuilder'] = 'https://cdn.datatables.net/searchbuilder/1.2.2/css/searchBuilder.dataTables.min.css';
-        $this->scriptIncludes['SearchBuilder'] = 'https://cdn.datatables.net/searchbuilder/1.2.2/js/dataTables.searchBuilder.min.js';
+        $this->linkIncludes['SearchBuilder'] = [];
+        $this->scriptIncludes['SearchBuilder'] = [];
+        $this->linkIncludes['SearchBuilder'][] = 'https://cdn.datatables.net/searchbuilder/1.2.2/css/searchBuilder.dataTables.min.css';
+        $this->scriptIncludes['SearchBuilder'][] = 'https://cdn.datatables.net/datetime/1.1.0/js/dataTables.dateTime.min.js';
+        $this->scriptIncludes['SearchBuilder'][] = 'https://cdn.datatables.net/searchbuilder/1.2.2/js/dataTables.searchBuilder.min.js';
         return $this;
     }
     public function extSearchPanes() {
-        $this->linkIncludes['SearchPanes'] = 'https://cdn.datatables.net/searchpanes/1.4.0/css/searchPanes.dataTables.min.css';
-        $this->scriptIncludes['SearchPanes'] = 'https://cdn.datatables.net/searchpanes/1.4.0/js/dataTables.searchPanes.min.js';
+        $this->linkIncludes['SearchPanes'] = [];
+        $this->scriptIncludes['SearchPanes'] = [];
+        $this->linkIncludes['SearchPanes'][] = 'https://cdn.datatables.net/searchpanes/1.4.0/css/searchPanes.dataTables.min.css';
+        $this->scriptIncludes['SearchPanes'][] = 'https://cdn.datatables.net/searchpanes/1.4.0/js/dataTables.searchPanes.min.js';
         return $this;
     }
     public function extSelect() {
-        $this->linkIncludes['Select'] = 'https://cdn.datatables.net/select/1.3.3/css/select.dataTables.min.css';
-        $this->scriptIncludes['Select'] = 'https://cdn.datatables.net/select/1.3.3/js/dataTables.select.min.js';
+        $this->linkIncludes['Select'] = [];
+        $this->scriptIncludes['Select'] = [];
+        $this->linkIncludes['Select'][] = 'https://cdn.datatables.net/select/1.3.3/css/select.dataTables.min.css';
+        $this->scriptIncludes['Select'][] = 'https://cdn.datatables.net/select/1.3.3/js/dataTables.select.min.js';
         return $this;
     }
 
@@ -158,14 +201,80 @@ class DataTable {
         return $this;
     }
 
+    public function addTableClass($class) {
+        $this->tableClasses .= $class . " ";
+    }
+    public function removeTableClass($class) {
+        // TODO
+    }
     public function addHeader($header, $attributes=[]) {
         $this->headers[][$header] = $attributes;
+    }
+    public function setHeaders($headers) {
+        foreach ($headers as $header => $attributes) {
+            $this->headers[][$header] = $attributes;
+        }
+    }
+    public function addRow($row) {
+        $this->data[] = $row;
     }
     public function addOption($option) {
         $this->options[] = $option;
     }
-    public function printLinkIncludes() {}
-    public function create() {}
-    public function printScriptIncludes() {}
+    public function setOptions($options) {
+        $this->options = $options;
+    }
+    public function installLinkIncludes() {
+        foreach ($this->linkIncludes as $type => $urls) {
+            echo "<!-- START $type -->";
+            foreach ($urls as $index => $url) {
+                echo "<link href='$url' type='text/css' rel='stylesheet' />";
+            }
+            echo "<!-- END $type -->";
+        }
+        $this->installedLinkIncludes = true;
+    }
+    public function create() {
+        $html = "<table id='" . $this->identifer . "' class='" . $this->tableClasses . "'><thead><tr>";
+        foreach ($this->headers as $index => $headers) {
+            foreach ($headers as $header => $attributes) {
+                $attrs = '';
+                foreach ($attributes as $attributeKey => $attributeValue) {
+                    $attrs .= "$attributeKey='$attributeValue' ";
+                }
+                $html .= "<th $attrs>$header</th>";
+            }
+        }
+        $html .= "</tr></thead>";
+
+        foreach ($this->data as $row) {
+            $html .= $row;
+        }
+        $html .= "</table>";
+        return $html;
+    }
+    public function installScriptIncludes() {
+        echo "<!-- START jQuery -->";
+        echo '<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>';
+        echo "<!-- END jQuery -->";
+        foreach ($this->scriptIncludes as $type => $urls) {
+            echo "<!-- START $type -->";
+            foreach ($urls as $index => $url) {
+                echo "<script src='$url' rel='script'></script>";
+            }
+            echo "<!-- END $type -->";
+        }
+        ?>
+<script>
+    var table_<?= $this->identifer ?> = $('#<?= $this->identifer ?>').DataTable().options({
+        <?= !empty($this->options) ? json_encode($this->options, JSON_PRETTY_PRINT) : ''; ?>
+    });
+</script>
+        <?php
+        $this->installedScriptIncludes = true;
+    }
     public function destroy() {}
 }
--- DataTables/examples.php
+++ DataTables/examples.php
@@ -1,1 +1,112 @@
 <?php
+use DataTables\DataTable;
+include_once 'DataTable.php';
+$datatable = new DataTable('datatable');
+$datatable->extFixedHeader()->extButtons()->extSearchBuilder();
+$dummyData = '[
+	{
+		"name": "Kuame Boyd",
+		"phone": "1-854-577-4725",
+		"email": "rutrum.fusce@necimperdiet.edu",
+		"address": "713-723 Quisque Street",
+		"postalZip": "187459",
+		"region": "San Andrés y Providencia",
+		"country": "Chile"
+	},
+	{
+		"name": "Audra Lynch",
+		"phone": "1-232-576-8133",
+		"email": "nec.euismod.in@sit.edu",
+		"address": "Ap #712-2760 Dictum St.",
+		"postalZip": "4380",
+		"region": "Vorarlberg",
+		"country": "New Zealand"
+	},
+	{
+		"name": "Lionel Travis",
+		"phone": "1-273-413-6819",
+		"email": "neque.pellentesque@et.org",
+		"address": "790-727 Sed Avenue",
+		"postalZip": "2799 IW",
+		"region": "Jigawa",
+		"country": "Poland"
+	},
+	{
+		"name": "Charissa Goodwin",
+		"phone": "1-531-275-1823",
+		"email": "dictum@ametrisus.co.uk",
+		"address": "Ap #671-805 Sapien Ave",
+		"postalZip": "3396",
+		"region": "Lipetsk Oblast",
+		"country": "United Kingdom"
+	},
+	{
+		"name": "Hamish Gibbs",
+		"phone": "1-500-459-5313",
+		"email": "risus@aliquameratvolutpat.edu",
+		"address": "Ap #579-5526 Ligula. St.",
+		"postalZip": "9691",
+		"region": "Vienna",
+		"country": "Mexico"
+	},
+	{
+		"name": "Gisela Nelson",
+		"phone": "(381) 816-6988",
+		"email": "dolor.sit@curabiturmassa.com",
+		"address": "499-8848 Commodo Avenue",
+		"postalZip": "62711",
+		"region": "Tây Ninh",
+		"country": "United Kingdom"
+	},
+	{
+		"name": "Darrel Cline",
+		"phone": "1-594-654-8724",
+		"email": "dolor.tempus@tellusphaselluselit.org",
+		"address": "5374 Semper. Ave",
+		"postalZip": "81484",
+		"region": "Emilia-Romagna",
+		"country": "Italy"
+	},
+	{
+		"name": "Gray Fields",
+		"phone": "(742) 633-4337",
+		"email": "diam@feugiattellus.edu",
+		"address": "8950 Ut Av.",
+		"postalZip": "34338",
+		"region": "Jeju",
+		"country": "South Korea"
+	},
+	{
+		"name": "Alexa Eaton",
+		"phone": "1-372-828-7712",
+		"email": "enim@estmauris.ca",
+		"address": "Ap #549-9949 Ipsum Avenue",
+		"postalZip": "58234",
+		"region": "South Gyeongsang",
+		"country": "Germany"
+	},
+	{
+		"name": "Ciaran Kirkland",
+		"phone": "(284) 864-8625",
+		"email": "bibendum.ullamcorper.duis@justoeuarcu.edu",
+		"address": "247-8303 Ullamcorper, St.",
+		"postalZip": "11108",
+		"region": "Connecticut",
+		"country": "Indonesia"
+	}
+]';
+$datas = json_decode($dummyData, true);
+$datatable->setHeaders(["Name" => [], "Phone" => [], "Email" => [], "Address" => [], "Postal" => [], "Region" => [], "Country" => []]);
+foreach ($datas as $row) {
+    $name = $row['name'];
+    $phone = $row['phone'];
+    $email = $row['email'];
+    $address = $row['address'];
+    $postal = $row['postalZip'];
+    $region = $row['region'];
+    $country = $row['country'];
+    $datatable->addRow("<tr><td>$name</td><td>$phone</td><td>$email</td><td>$address</td><td>$postal</td><td>$region</td><td>$country</td></tr>");
+}
+$datatable->installLinkIncludes();
+echo $datatable->create();
+$datatable->installScriptIncludes();
